509f569bd411eb469f31e31a66ef735640cafa8e
Irina O'Hara <irina.v.ohara@gmail.com>
2018-10-03 20:51:09 -0600

    add form

--- web/submitted-form.php
+++ web/submitted-form.php
@@ -1,6 +1,17 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
 <?php 
 	<?php echo $_GET["name"]; ?><br>
 	<?php echo $_GET["email"]; ?><br>
 	<?php echo $_GET["major"]; ?><br>
 	<?php echo $_GET["comments"]; ?><br>
+
  ?>
+ 
+</body>
+</html>
