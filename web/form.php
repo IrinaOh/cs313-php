bd9da3080d74addb1f404c28ae3dbba24ad44cbf
Irina O'Hara <irina.v.ohara@gmail.com>
2018-10-03 21:54:52 -0600

    add form

--- web/form.php
+++ web/form.php
@@ -10,13 +10,30 @@
 		<label>Email:</label>
 		<input type="text" name="email" value="email"><br>
 		<label>Major:</label>
-		<input type="radio" name="major" value="cs">CS
+		<?php  
+			$majors = array("Computer Science"=>"cs", 
+				"Web Design and Development"=>"wdd", 
+				"Computer Information Tehnology" => "cit",
+				"computer Engeneering" => "ce");
+			foreach ($majors as $m => $m_code) {
+				print "<input type=\"radio\" name=\"major\" value=\"" . $m . "\" id=\"" . $m_code . "\"><label for=\"" . $m_code . "\">" . $m . "</label><br />";
+			}	
+		?>
+<!-- 		<input type="radio" name="major" value="cs">CS
 		<input type="radio" name="major" value="wdd">WDD
 		<input type="radio" name="major" value="cit">CIT
-		<input type="radio" name="major" value="ce">CE
+		<input type="radio" name="major" value="ce">CE -->
+
 		<br>
 		<label>Comments:</label>
-		<input type="text-area" name="comments" value=""><br>
+		<textarea name="comments"></textarea></br></br>
+        <input type="checkbox" name="continent[]" value="na">North America</br>
+        <input type="checkbox" name="continent[]" value="sa">South America</br>
+        <input type="checkbox" name="continent[]" value="eu">Europe</br>
+        <input type="checkbox" name="continent[]" value="as">Asia</br>
+        <input type="checkbox" name="continent[]" value="au">Austrailia</br>
+        <input type="checkbox" name="continent[]" value="af">Africa</br>
+        <input type="checkbox" name="continent[]" value="an">Antarctica</br>
 		<input type="submit" name="submit">
 	</form>
 </body>
--- web/submitted-form.php
+++ web/submitted-form.php
@@ -10,11 +10,32 @@
 	$email = $_POST["email"];
 	$major = $_POST["major"];
 	$comments = $_POST["comments"];
+	$continents = $_POST["continent"];
 	echo  "$name <br>";
 	echo "<a href='mailto:" . $email . "'>";
-	echo  "$name <br>";
-	echo  "$name <br>";
+	echo  "$major <br>";
+	echo  "$comments <br>";
 ?>
+<html>
+<body>
+<p>Name: <?=$name ?></p>
+<p>Email: <a href="mailto:<?=$email ?>"><?=$email ?></a></p>
+<p>Major: <?=$major ?></p>
+<p>Comments: <?=$comments ?></p>
+<p>Continents:</br>
+	<?php
+		$continentsDB = array("na"=>"North America", 
+							"sa"=>"South America",
+							"eu"=>"Europe",
+							"as"=>"Asia",
+							"au"=>"Australia",
+							"af"=>"Africa",
+							"an"=>"Antartica");
+		foreach ($continents as $c){
+			print "$continentsDB[$c] <br />";
+		}
+	?>
+</p>
 
 </body>
 </html>
