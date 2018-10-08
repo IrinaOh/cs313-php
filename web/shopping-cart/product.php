523a0357d91e953691cd2f927c304d74da4cf9ef
Irina O'Hara <irina.v.ohara@gmail.com>
2018-10-07 23:17:18 -0600

    add new code

--- web/shopping-cart/product.php
+++ web/shopping-cart/product.php
@@ -1,12 +1,12 @@
-<?php
-  session_start();
-  if (isset($_SESSION["cart"]) < 1) {
-    $_SESSION["cart"] = array();
-  }
-  $_SESSION["product1"] = $_POST["product1"];
-   $_SESSION["product2"] = $_POST["product2"];
-   $_SESSION["product3"] = $_POST["product3"];
-  print($_SESSION["product1"])
+<?php 
+	if(isset($_POST['submit'])){
+		session_start(); 
+		$_SESSION['salt'] = htmlentities($_POST['salt']);
+		$_SESSION['pepper'] = htmlentities($_POST['pepper']);
+		$_SESSION['rosemary'] = htmlentities($_POST['rosemary']);
+		$_SESSION['cumin'] = htmlentities($_POST['cumin']);
+		header('Location: shopping-cart.php');
+	}
 ?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
@@ -15,27 +15,14 @@
     <title></title>
   </head>
   <body>
-    <div class="">
-      <p>Product 1</p>
-      <form class="" action="product.php" method="post">
-        <input type="hidden" name="product1">
-        <input type="submit" name="submit" value="Add to Cart">
-      </form>
-    </div>
-    <div class="">
-      <p>Product 2</p>
-      <form class="" action="product.php" method="post">
-        <input type="hidden" name="product2">
-        <input type="submit" name="submit" value="Add to Cart">
-      </form>
-    </div>
-    <div class="">
-      <p>Product 3</p>
-      <form class="" action="product.php" method="post">
-        <input type="hidden" name="product3">
-        <input type="submit" name="submit" value="Add to Cart">
-      </form>
-    </div>
-    <a href="shopping-cart.php">SEE SHOPPING CART</a>
+  	<h2>Welcome to the spices shop.</h2>
+  	<h4>Pick your spices:</h4>
+	<form method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+		<input type="checkbox" name="salt" value="salt"> Salt<br>
+		<input type="pepper" name="pepper" value="pepper"> Pepper<br>
+		<input type="rosemary" name="rosemary" value="rosemary"> Rosemary<br>
+		<input type="cumin" name="cumin" value="cumin"> Cumin<br>
+		<input type="submit" name="submit" value="submit">
+	</form>
   </body>
 </html>
--- web/shopping-cart/shopping-cart.php
+++ web/shopping-cart/shopping-cart.php
@@ -1,31 +1,25 @@
-<?php session_start(); ?>
-
-<?php
-	$cart = $_SESSION["cart"];
-	$product1 = $_SESSION["product1"]; 
-	$product2 = $_SESSION["product2"];
-	$product3 = $_SESSION["product3"];
+<?php  
+	session_start();
+	$salt = $_SESSION['salt'];
+	$pepper = $_SESSION['pepper'];
+	$rosemary = $_SESSION['rosemary'];
+	$cumin = $_SESSION['cumin'];
 ?>
-
+<!DOCTYPE html>
 <html>
+<head>
+	<title></title>
+</head>
 <body>
+	<h5>Items in your shopping cart:</h5>
+	<?php 
+	echo $salt; 
+	echo $pepper;
+	echo $rosemary;
+	echo $cumin;
 
-<p>Product1: <?=$product1 ?></p><br>
-<p>Product2: <?=$product2 ?></p><br>
-<p>Product3: <?=$product3 ?></p><br>
-
-<?php
-	$productsDB = array("001"=>"Product 1",
-						"002"=>"Product 2",
-						"003"=>"Product 3");
-	foreach ($cart as $p){
-		print "$productsDB[$p]";
-		print "$cart[$p] <br />";
-	}
-?>
-<p>Remove all items from the cart:</p> <?php session_unset(); ?>
-
-<a href="product.php">BACK TO SHOPPING</a>
-<a href="checkout.php">PROCEED TO CHECKOUT</a>
+	?>
+	<a href="product.php">BACK TO SHOPPING</a>
+	<a href="checkout.php">PROCEED TO CHECKOUT</a>
 </body>
 </html>
