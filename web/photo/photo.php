3bf2f0c89145ba8caeb7df05d60ffc2be374aa5d
Irina O'Hara <irina.v.ohara@gmail.com>
2018-10-20 22:51:48 -0600

    add display data

---
+++ web/photo/photo.php
@@ -0,0 +1,51 @@
+<?php
+try
+{
+  $dbUrl = getenv('DATABASE_URL');
+
+  $dbOpts = parse_url($dbUrl);
+
+  $dbHost = $dbOpts["host"];
+  $dbPort = $dbOpts["port"];
+  $dbUser = $dbOpts["user"];
+  $dbPassword = $dbOpts["pass"];
+  $dbName = ltrim($dbOpts["path"],'/');
+
+  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $ex)
+{
+  echo 'Error!: ' . $ex->getMessage();
+  die();
+}
+?>
+<!doctype html>
+<head>
+	<title>Photo Finder</title>
+</head>
+<body>
+	<h1> Photo Sessions </h1>
+	<?php
+	foreach ($db->query('SELECT * FROM schedules') as $row)
+		{
+		  echo "<p><b>" . $row['schedule_date'] . " ";
+		  echo $row['schedule_time'];
+		  echo ":" . $row['location'] . "</b> - ";
+		  echo '</p>';
+	}
+	?>
+
+	<form action="" method ="post" id="searchForm">
+		<input type="text" name="search">
+		<input type="submit" name="submit" value="Search">
+	</form>
+
+	<?php
+
+	?>
+
+
+</body>
+</html>
