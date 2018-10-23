aad39a5487491545e7e5d34c6d8a86de3a25c906
Irina O'Hara <irina.v.ohara@gmail.com>
2018-10-22 23:16:26 -0600

    display ids

--- web/photo/photo.php
+++ web/photo/photo.php
@@ -1,25 +1,18 @@
-<?php 
-	session_start();
-	$_SESSION['sid'] = $_GET['id'];
-	$sid = $_GET['id'];
+<?php
+  $_GET['id'] = $id; 
 ?>
-
 
 <?php
 try
 {
   $dbUrl = getenv('DATABASE_URL');
-
   $dbOpts = parse_url($dbUrl);
-
   $dbHost = $dbOpts["host"];
   $dbPort = $dbOpts["port"];
   $dbUser = $dbOpts["user"];
   $dbPassword = $dbOpts["pass"];
   $dbName = ltrim($dbOpts["path"],'/');
-
   $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-
   $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch (PDOException $ex)
@@ -30,23 +23,18 @@ catch (PDOException $ex)
 ?>
 <!doctype html>
 <head>
-	<title>Photo Finder</title>
+  <title>Photoshoot Finder</title>
 </head>
 <body>
-	<h1> Photo Sessions </h1>
-	<?php
-		foreach ($db->query('SELECT photoshoot_type FROM photoshoot') as $p)
-		{
-			echo "<p><a href='session-detail.php?id=" . $p['photoshoot_id'] . "'>" . "</a></p>";
-		}
-	?>
-
-<!-- 	<form action="" method ="post" id="searchForm">
-		<input type="text" name="search">
-		<input type="submit" name="submit" value="Search">
-	</form> -->
-
-
+  <h1> Photo Sessions </h1>
+  <?php
+  foreach ($db->query('SELECT photoshoot_id, photoshoot_type FROM photoshoot') as $p)
+  {
+    $id = $p['photoshoot_id'];
+    $type = $p['photoshoot_type'];
+    echo "<p><a href='session-detail.php?id=$id'>" . $type . "</a></p>";
+  }
+  ?>
 
 </body>
 </html>
