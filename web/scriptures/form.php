85c0ff7c8611ffdd7c18b4d3076fe8150da0f3e7
Irina O'Hara <irina.v.ohara@gmail.com>
2018-10-24 20:42:11 -0600

    add form

--- web/scriptures/form.php
+++ web/scriptures/form.php
@@ -20,14 +20,14 @@
           <li>Chapter: <input type="text" name="chapter"></li>
           <li>Verse: <input type="text" name="verse"></li>
           <li>Content: <input type="textarea" name="content"></li>
-          <li>Topic: 
-            foreach ($db->query('SELECT name FROM topic') as $row)
-              {
-                $name = $row['name'];
-                echo "<input type='checkbox' name='" . $name . "' value='" . $name . "'><br>";
-            }
+          // <li>Topic: 
+          //   foreach ($db->query('SELECT name FROM topic') as $row)
+          //     {
+          //       $name = $row['name'];
+          //       echo "<input type='checkbox' name='" . $name . "' value='" . $name . "'><br>";
+          //   }
            
-          </li>
+          // </li>
           <li><input type="submit" name="submit" value="submit"></li>
         </form>
       </ul>
