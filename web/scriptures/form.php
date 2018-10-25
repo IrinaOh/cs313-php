df442e3d680393ce9753f84a7ecd6f11c76d137f
Irina O'Hara <irina.v.ohara@gmail.com>
2018-10-24 20:44:59 -0600

    add file

--- web/scriptures/form.php
+++ web/scriptures/form.php
@@ -1,6 +1,30 @@
 <?php
-  require ('dbConnect.php');
-  $db = get_db();
+  //require ('dbConnect.php');
+  //$db = NULL;
+
+  try
+  {
+    $dbUrl = getenv('DATABASE_URL');
+
+    $dbOpts = parse_url($dbUrl);
+
+    $dbHost = $dbOpts["host"];
+    $dbPort = $dbOpts["port"];
+    $dbUser = $dbOpts["user"];
+    $dbPassword = $dbOpts["pass"];
+    $dbName = ltrim($dbOpts["path"],'/');
+
+    $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  }
+  catch (PDOException $ex)
+  {
+    echo 'Error!: ' . $ex->getMessage();
+    die();
+  }
+}
+  //$db = get_db();
 
   //$query = 'SELECT s.book, s.chapter, s.verse, s.content, t.name FROM scripture s JOIN topic t ON s.topic_id = t.topic_id';
   // $stmt = $db->prepare($query);
