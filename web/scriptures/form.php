dd5273c4deb08fa2ca74b23d776da615dbefcde4
Irina O'Hara <irina.v.ohara@gmail.com>
2018-10-24 21:27:20 -0600

    add form

--- web/scriptures/form.php
+++ web/scriptures/form.php
@@ -25,20 +25,21 @@
 </head>
 <body>
 	<h1>Add New Scriptures</h1>
-        <form action="insert.php" method ="POST">
-          Book: <input type="text" name="book"/></br>
-          Chapter: <input type="text" name="chapter"/></br>
-          Verse: <input type="text" name="verse"/></br>
-          Content: <input type="textarea" name="content"/></br>
-          Topic:
-          <?php  
-          foreach ($db->query('SELECT name FROM topic') as $row)
-              {
-                echo "<input type = 'checkbox' name= 'topic' value=" . $row['name'] . ">". $row['name'] ."</br>"
-                ;
-          }
-          ?>
-          <input type="submit" name="submit" value="submit"/>
-        </form>
+  <form action="insert.php" method ="POST">
+    Book: <input type="text" name="book"/></br>
+    Chapter: <input type="text" name="chapter"/></br>
+    Verse: <input type="text" name="verse"/></br>
+    Content: <input type="textarea" name="content"/></br>
+    Topic:
+    <?php  
+    foreach ($db->query('SELECT name FROM topic') as $row)
+        {
+          $name = $row['name'];
+          echo "<input type = 'checkbox' name= 'topic' value=" . $name . ">". $name ."</br>"
+          ;
+    }
+    ?>
+    <input type="submit" name="submit" value="submit"/>
+  </form>
 </body>
 </html>
