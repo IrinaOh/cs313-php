c94bb6fedc7fbb353ef4a297ba1c5d06bf1f9223
Irina O'Hara <irina.v.ohara@gmail.com>
2018-10-29 22:32:19 -0600

    add form

--- web/scriptures/form.php
+++ web/scriptures/form.php
@@ -1,21 +1,7 @@
 <?php
-  try
-  {
-    $dbUrl = getenv('DATABASE_URL');
-    $dbOpts = parse_url($dbUrl);
-    $dbHost = $dbOpts["host"];
-    $dbPort = $dbOpts["port"];
-    $dbUser = $dbOpts["user"];
-    $dbPassword = $dbOpts["pass"];
-    $dbName = ltrim($dbOpts["path"],'/');
-    $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  }
-  catch (PDOException $ex)
-  {
-    echo 'Error!: ' . $ex->getMessage();
-    die();
-  }
+
+  require ('dbConnect.php');
+  $db = get_db();
 
 ?>
 <!doctype html>
@@ -33,10 +19,10 @@
     Topic:
     <?php  
     foreach ($db->query('SELECT name FROM topic') as $row)
-        {
-          $name = $row['name'];
-          echo "<input type = 'checkbox' name= 'topic' value=" . $name . ">". $name ."</br>"
-          ;
+    {
+      $name = $row['name'];
+      echo "<input type = 'checkbox' name= 'topic' value=" . $name . ">". $name ."</br>"
+      ;
     }
     ?>
     <input type="submit" name="submit" value="submit"/>
