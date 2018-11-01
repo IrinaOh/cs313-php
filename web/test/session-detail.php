d788a8579a789a68e41e237ebf7f78e8d5f2a66e
Irina O'Hara <irina.v.ohara@gmail.com>
2018-11-01 12:32:10 -0600

    add another query on a page

--- web/test/session-detail.php
+++ web/test/session-detail.php
@@ -7,6 +7,11 @@
 <?php
 require('dbConnect.php');
 $db = get_db();
+$photo_query = 'SELECT  photoshoot_type, photoshoot_length, photoshoot_number_of_people, photoshoot_number_of_images, photoshoot_number_of_outfits FROM photoshoot WHERE photoshoot_id='.$photoshoot_id;
+$photo_stmt = $db->prepare($photo_query);
+$photo_stmt->execute();
+$details = $photo_stmt->fetchAll(PDO::FETCH_ASSOC);
+
 //check if this query works in sql
 $query = 'SELECT p.photoshoot_type, p.photoshoot_length, p.photoshoot_number_of_people, p.photoshoot_number_of_images, p.photoshoot_number_of_outfits, f.feedback_content FROM feedback f JOIN photoshoot p ON f.feedback_photoshoot_id = p.photoshoot_id WHERE p.photoshoot_id='.$photoshoot_id;
 $stmt = $db->prepare($query);
@@ -24,17 +29,16 @@ $photoshoot_type = $photoshoots[0]['p.photoshoot_type'];
 <body>
 	<h1> Photo Session Details </h1>
 	<?php
-		foreach ($photoshoots as $p)
-		{
-		    $id = $p['photoshoot_id'];
-		    $type = $p['photoshoot_type'];
-		    $length = $p['photoshoot_length'];
-		    $people = $p['photoshoot_number_of_people'];
-			$images = $p['photoshoot_number_of_images'];
-		    $outfits = $p['photoshoot_number_of_outfits'];
 
-			echo "<p><b>" . $type . "</b> length: " . $length . " hour,<br> up to " . $people . " people,<br> " . $images . " images,<br> " . $outfits . " outfits</p>";
-		}
+	    $id = $details['photoshoot_id'];
+	    $type = $details['photoshoot_type'];
+	    $length = $details['photoshoot_length'];
+	    $people = $details['photoshoot_number_of_people'];
+		$images = $details['photoshoot_number_of_images'];
+	    $outfits = $details['photoshoot_number_of_outfits'];
+
+		echo "<p><b>" . $type . "</b> length: " . $length . " hour,<br> up to " . $people . " people,<br> " . $images . " images,<br> " . $outfits . " outfits</p>";
+
 	?>
 	<h2>Customer Feedback:</h2>
 	<?php
@@ -45,7 +49,7 @@ $photoshoot_type = $photoshoots[0]['p.photoshoot_type'];
 			echo "<p>" . $feedback . "</p>";
 		}
 	?>
-	<h2>Leave a comment:</h2>
+	<h2>Leave Your Feedback:</h2>
 	<form method="post" action="insert_feedback.php">
 		<input type="hidden" name="feedback_photoshoot_id" value="<?php echo $photoshoot_id;?>">
 		<input type="textarea" name="feedback_content">
