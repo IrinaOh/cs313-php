cee154fc8d7a4195088b372b919c477d081c4558
Irina O'Hara <irina.v.ohara@gmail.com>
2018-11-01 13:05:25 -0600

    test

--- web/test/session-detail.php
+++ web/test/session-detail.php
@@ -7,11 +7,6 @@
 <?php
 require('dbConnect.php');
 $db = get_db();
-$photo_query = 'SELECT photoshoot_type, photoshoot_length, photoshoot_number_of_people, photoshoot_number_of_images, photoshoot_number_of_outfits FROM photoshoot WHERE photoshoot_id='.$photoshoot_id;
-$photo_stmt = $db->prepare($photo_query);
-$photo_stmt->execute();
-$details = $photo_stmt->fetchAll(PDO::FETCH_ASSOC);
-$details_type = $details[0]['photoshoot_type'];
 
 //check if this query works in sql
 $query = 'SELECT p.photoshoot_type, p.photoshoot_length, p.photoshoot_number_of_people, p.photoshoot_number_of_images, p.photoshoot_number_of_outfits, f.feedback_content FROM feedback f JOIN photoshoot p ON f.feedback_photoshoot_id = p.photoshoot_id WHERE p.photoshoot_id='.$photoshoot_id;
@@ -28,27 +23,22 @@ $photoshoot_type = $photoshoots[0]['p.photoshoot_type'];
 	<title>Photoshoot Details</title>
 </head>
 <body>
-	<h1> Photo Session Details </h1>
+	<h1> <?php echo $photoshoot_type; ?>Photo Session Details </h1>
 	<?php
 
-	    $id = $details['photoshoot_id'];
-	    $type = $details['photoshoot_type'];
-	    $length = $details['photoshoot_length'];
-	    $people = $details['photoshoot_number_of_people'];
-		$images = $details['photoshoot_number_of_images'];
-	    $outfits = $details['photoshoot_number_of_outfits'];
+	    $id = $photoshoots['photoshoot_id'];
+	    $type = $photoshoots['photoshoot_type'];
+	    $length = $photoshoots['photoshoot_length'];
+	    $people = $photoshoots['photoshoot_number_of_people'];
+		$images = $photoshoots['photoshoot_number_of_images'];
+	    $outfits = $photoshoots['photoshoot_number_of_outfits'];
+	    $feedback = $photoshoots['feedback_content'];
 
 		echo "<p><b>" . $type . "</b> length: " . $length . " hour,<br> up to " . $people . " people,<br> " . $images . " images,<br> " . $outfits . " outfits</p>";
-
-	?>
-	<h2>Customer Feedback:</h2>
+		echo = "<h2>Customer Feedback:</h2>" . <?php foreach($photoshoots as $p){echo "<p>" . $feedback . "</p>";};?>
+	
 	<?php
-		foreach($photoshoots as $p)
-		{
-		    $feedback = $p['feedback_content'];
 
-			echo "<p>" . $feedback . "</p>";
-		}
 	?>
 	<h2>Leave Your Feedback:</h2>
 	<form method="post" action="insert_feedback.php">
