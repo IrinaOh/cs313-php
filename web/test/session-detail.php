64be3b007464591da8837b1c2b54fbfe52fd4cac
Irina O'Hara <irina.v.ohara@gmail.com>
2018-11-01 13:40:20 -0600

    test

--- web/test/session-detail.php
+++ web/test/session-detail.php
@@ -11,11 +11,11 @@ $db = get_db();
 //check if this query works in sql
 $query = 'SELECT p.photoshoot_type, p.photoshoot_length, p.photoshoot_number_of_people, p.photoshoot_number_of_images, p.photoshoot_number_of_outfits, f.feedback_content FROM feedback f JOIN photoshoot p ON f.feedback_photoshoot_id = p.photoshoot_id WHERE p.photoshoot_id='.$photoshoot_id;
 $stmt = $db->prepare($query);
-$stmt->bindValue(':photoshoot_type', $photoshoot_type, PDO::PARAM_INT);
+// $stmt->bindValue(':p.photoshoot_id', $photoshoot_id, PDO::PARAM_INT);
 $stmt->execute();
 
 $photoshoots = $stmt->fetchAll(PDO::FETCH_ASSOC);
-// $photoshoot_type = $photoshoots[0]['p.photoshoot_type'];
+$photoshoot_type = $photoshoots[0]['p.photoshoot_type'];
 ?>
 
 <!doctype html>
