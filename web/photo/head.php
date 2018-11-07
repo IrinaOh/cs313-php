1c4dc3667b17e87eb81a2312f743fb4db7bd702a
Irina O'Hara <irina.v.ohara@gmail.com>
2018-11-06 21:40:45 -0700

    links

--- web/photo/head.php
+++ web/photo/head.php
@@ -1,7 +1,7 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta name="description" content="Portrait photography, individual, couples and families photography."/>
-<link rel="stylesheet" href="../css/header.css">
-<link rel="stylesheet" href="../css/fonts.css">
-<link rel="stylesheet" href="../css/form.css">
-<link rel="stylesheet" href="../css/index.css">
+<link rel="stylesheet" href="header.css">
+<link rel="stylesheet" href="fonts.css">
+<link rel="stylesheet" href="form.css">
+<link rel="stylesheet" href="index.css">
