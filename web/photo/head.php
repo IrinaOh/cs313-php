d3d2ef1c577601dfb3e75a610f1d6fe449869873
Irina O'Hara <irina.v.ohara@gmail.com>
2018-11-06 17:10:10 -0700

    add css

--- web/photo/head.php
+++ web/photo/head.php
@@ -4,4 +4,4 @@
 <link rel="stylesheet" href="../css/header.css">
 <link rel="stylesheet" href="../css/fonts.css">
 <link rel="stylesheet" href="../css/form.css">
-<link rel="stylesheet" href="../css/main.css">
+<link rel="stylesheet" href="../css/index.css">
